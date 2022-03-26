3349a415f17bcf3745efd54ca9f2022b20212862
utsho <utsho111099@gmail.com>
2022-03-26 18:10:14 +0600

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Shoes from './Components/shoes/Shoes';
+
+function App() {
+    return (
+        <div className="App">
+            <Shoes></Shoes>
+            <Shoes></Shoes>
+            <Shoes></Shoes>
+        </div>
+    );
+}
+
+export default App;
